2e06ad69b415af5e902739009b8ecbfebd297483
SlyceCode <ropon.coder@outlook.com>
2024-10-23 21:27:57 -0500

    Creacion del Proyecto VS

---
+++ finalGame/finalGame/menu.cpp
@@ -0,0 +1,10 @@
+#include "menu.h"
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^ arg)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew finalGame::menu());
+}
---
+++ finalGame/finalGame/menu.h
@@ -0,0 +1,68 @@
+#pragma once
+
+namespace finalGame {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de menu
+	/// </summary>
+	public ref class menu : public System::Windows::Forms::Form
+	{
+	public:
+		menu(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~menu()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// menu
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1280, 720);
+			this->Name = L"menu";
+			this->Text = L"menu";
+			this->Load += gcnew System::EventHandler(this, &menu::menu_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void menu_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
